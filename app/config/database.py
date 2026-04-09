f0f1ecf3f6057181e43177ae6ff277df53dae765
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2026-04-10 01:41:54 +0500

    Add a rate limiting

--- app/config/database.py
+++ app/config/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from settings import settings
+from core.settings import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,25 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from pyrate_limiter import Duration, Limiter, Rate
+from fastapi_limiter.depends import RateLimiter
 
 app = FastAPI()
 
+origins = [
+"http://localhost:8080",
+]
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins,
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
+
+@app.get(
+"/",
+dependencies=[Depends(RateLimiter(limiter=Limiter(Rate(2, Duration.SECOND * 5))))],
+)
+async def index():
+    return {"msg": "Hello World"}
