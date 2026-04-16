6e8bfe92457a838e077287238b03fb06a353f82c
Saqib Younas <muhammadsaqibyounas11@gmail.com>
2026-04-17 01:26:14 +0500

    update the code

--- app/models/auth.py
+++ app/models/auth.py
@@ -4,15 +4,13 @@ from uuid import uuid4
 from datetime import datetime
 import enum
 
-# Enums SQLModel ke saath
 class CompanySize(str, enum.Enum):
     small = "small"
     medium = "medium"
     large = "large"
 
-# Company Model
 class Company(SQLModel, table=True):
-    __tablename__ = "companies" # Optional, SQLModel khud handle kar leta hai
+    __tablename__ = "companies"
 
     id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
     company_name: str = Field(index=True)
@@ -22,10 +20,8 @@ class Company(SQLModel, table=True):
     location: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
     
-    # Relationship: Ek company mein many users ho sakte hain
     users: List["User"] = Relationship(back_populates="company")
 
-# User Model
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
@@ -34,15 +30,10 @@ class User(SQLModel, table=True):
     email: str = Field(unique=True, index=True)
     password: str
     
-    # Foreign Key
     company_id: Optional[str] = Field(default=None, foreign_key="companies.id")
-    
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    
-    # Relationship: User ek hi company se belong karega
     company: Optional[Company] = Relationship(back_populates="users")
 
-# DB Creation function
 from app.config.dbconnection import engine
 
 def create_db():
